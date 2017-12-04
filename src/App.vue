e5273cbefffc9bceac941bb4eba47a65eb17148b
catlover91 <7215827+CatLover91@users.noreply.github.com>
2017-12-04 21:49:58 +0000

    cleaning up scss

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,5 @@ export default {
 }
 </script>
 <style lang="scss">
-@import '../../patience-style/patience.scss';
 
 </style>
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import 'hack'
 
 new Vue({
   el: '#app',
